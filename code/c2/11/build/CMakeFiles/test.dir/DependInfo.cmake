cd8de04ed572a4e3a35393f247155dc982f339b8
ctai <ctai@mozilla.com>
2015-11-08 21:39:38 +0800

    TDD example

---
+++ code/c2/11/build/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/ctai/github/TDD/code/c2/11/SoundexTest.cpp" "/Users/ctai/github/TDD/code/c2/11/build/CMakeFiles/test.dir/SoundexTest.cpp.o"
+  "/Users/ctai/github/TDD/code/c2/11/main.cpp" "/Users/ctai/github/TDD/code/c2/11/build/CMakeFiles/test.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/ctai/gmock-1.7.0/include"
+  "/Users/ctai/gmock-1.7.0/gtest/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
